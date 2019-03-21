4010eee83055843da2ecc8fff98d0887f97956de
xtianmarucco <christian.marucco@gmail.com>
2019-03-21 17:37:35 -0300

    sdasd

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-
+import { Router } from "@angular/router"
 
 @Component({
   selector: 'app-register',
@@ -10,13 +10,27 @@ import { AuthService } from '../../services/auth.service';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private router: Router,
+  ) { }
+
+
+
 
   ngOnInit() {
   }
- register(form) {
-  let body = {email: form.value.email, name: form.value.name, password: form.value.password, role: form.value.role};
-  let resp = this.auth.register(body);
- }
-  
+
+  register(form) {
+    let body = { email: form.value.email, name: form.value.name, password: form.value.password, role: form.value.role };
+
+    let resp = this.auth.register(body);
+    resp.subscribe(value => {
+      if (value) {
+        localStorage.setItem('token', value.access_token);
+        this.router.navigate(['/home'])
+      }
+    })
+
+  }
+
+
 }
